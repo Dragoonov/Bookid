e0d9079c7435de115d9b4dcaa56841b7f02997bf
Dragoonov <dragovonnova@gmail.com>
2020-10-14 21:29:42 +0200

    Started tests

--- app/src/test/java/com/moonlightbutterfly/bookid/repository/internalrepo/RoomRepositoryFake.kt
+++ app/src/test/java/com/moonlightbutterfly/bookid/repository/internalrepo/RoomRepositoryFake.kt
@@ -2,6 +2,9 @@ package com.moonlightbutterfly.bookid.repository.internalrepo
 
 import com.moonlightbutterfly.bookid.repository.database.entities.Shelf
 import com.moonlightbutterfly.bookid.repository.database.entities.User
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import io.reactivex.Single
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 
@@ -12,43 +15,78 @@ class RoomRepositoryFake : InternalRepository {
 
     private var user: User? = null
 
-    override suspend fun insertShelf(shelf: Shelf): Unit = run {
+    override fun insertShelf(shelf: Shelf): Completable {
         if(shelfs == null) shelfs = ArrayList()
-        shelfs?.add(shelf)!!
+        return Completable.fromCallable {
+            shelfs?.add(shelf)
+        }
+
     }
 
-    override suspend fun updateShelf(shelf: Shelf): Unit = run {
+    override fun updateShelf(shelf: Shelf): Completable {
         if(shelfs == null) shelfs = ArrayList()
-        shelfs?.removeIf { shelf.id == it.id }.also {
-            if (it!!) {
-                shelfs?.add(shelf)
+        return Completable.fromCallable {
+            shelfs?.removeIf { shelf.id == it.id }.also {
+                if (it!!) {
+                    shelfs?.add(shelf)
+                }
             }
         }
     }
 
-    override suspend fun deleteShelf(shelf: Shelf): Unit = run {
-        shelfs?.removeIf { shelf.id == it.id }
-    }
+    override fun deleteShelf(shelf: Shelf) =
+       Completable.fromCallable { shelfs?.removeIf { shelf.id == it.id } }
 
-    override suspend fun getShelfById(id: Int): Shelf? = shelfs?.find { id == it.id }
+    override fun getShelfById(id: Int, userId: String): Flowable<Shelf?> = Flowable.just(
+        shelfs?.find {
+            id == it.id && userId == it.userId
+        }
+    )
 
-    override suspend fun getShelfByName(name: String): Shelf? = shelfs?.find { it.name == name }
+    override fun getShelfByName(name: String, userId: String): Single<Shelf?> = Single.just(shelfs?.find {
+        it.name == name && userId == it.userId
+    })
 
-    override suspend fun getShelfes(): List<Shelf>? = shelfs
+    override fun getShelfByBaseId(id: Int, userId: String): Flowable<Shelf?> = Flowable.just(
+        shelfs?.find { it.baseShelfId == id && userId == it.userId }
+    )
 
-    override fun getUserShelfs(userId: String): Flow<List<Shelf>?> = flowOf(shelfs?.toList())
+    override fun doBaseShelfsExist(id: Int, userId: String): Single<Shelf> = Single.just(
+        shelfs?.find { it.baseShelfId == id && userId == it.userId }
+    )
 
-    override fun getUser(): Flow<User?> = flowOf(
+    override fun getShelfes(): Single<List<Shelf>?> = Single.just(shelfs)
+
+    override fun getUserShelfs(userId: String?): Flowable<List<Shelf>?> = Flowable.just(shelfs?.toList())
+    override fun getUser(): Flowable<User?> = Flowable.just(
         User(
-            "116644458345052983935",
+            "11",
             "Jakub Lipowski",
             "jakub.lipowski01@gmail.com",
             "https://lh3.googleusercontent.com/a-/AOh14GiPou93h951L-XfDmexoG3YKIFM1e7zsNzl5a4B"
         )
     )
 
-    override suspend fun insertUser(user: User): Unit = run { this.user = user }
+    override fun getUserById(id: String): Flowable<List<User>> = Flowable.just(
+        if (id == this.user?.id) {
+            listOf(this.user!!)
+        } else {
+            listOf()
+        }
+    )
+    override fun insertUser(user: User): Completable = Completable.fromCallable {
+        this.user = user
+        user
+    }
 
-    override suspend fun deleteUser(user: User) = run { this.user = null }
+    override fun updateUser(user: User): Completable = Completable.fromCallable {
+        this.user = user
+        user
+    }
+
+    override fun deleteUser(user: User) = Completable.fromCallable {
+        this.user = null
+        null
+    }
 
 }
